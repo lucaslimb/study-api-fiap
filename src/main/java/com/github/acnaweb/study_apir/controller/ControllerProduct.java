f57d59af768d8aa0466a3ca9d9b6120030c58cdd
Lucas <lucasdelimabzr@gmail.com>
2025-02-27 22:39:18 -0300

    feat: crud

--- src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduct.java
+++ src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduct.java
@@ -7,29 +7,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.github.acnaweb.study_apir.service.ProductService;
 
 @RestController
 @RequestMapping("produtos")
 public class ControllerProduct {
 
+    private ProductService productService;
+
     @PostMapping
     public ResponseEntity<String> create() {
+        productService.createProduct(null);
         return ResponseEntity.status(201).body("Produto cadastrado");
     }
 
     @DeleteMapping
     public ResponseEntity<Void> delete() {
+        productService.deleteProduct(null);
         return ResponseEntity.status(204).build();
 
     }
 
     @PutMapping
     public ResponseEntity<String> update() {
+        productService.updateProduct(null, null);
         return ResponseEntity.status(200).body("Produto atualizado");        
     }
 
     @GetMapping
     public ResponseEntity<String> find() {
+        productService.getProductById(null);
         return ResponseEntity.status(200).body("Maça");        
 
     }
--- src/main/java/com/github/acnaweb/study_apir/service/ProductService.java
+++ src/main/java/com/github/acnaweb/study_apir/service/ProductService.java
@@ -5,5 +5,26 @@ import java.util.List;
 import com.github.acnaweb.study_apir.model.Product;
 
 public class ProductService {
+
     private List<Product> products;
+
+    public Product createProduct(Product product){
+        
+        return null;
+    }
+
+    public Product getProductById(Long id){
+        
+        return null;
+    }
+
+    public Product updateProduct(Long id, Product product){
+        
+        return null;
+    }
+
+    public void deleteProduct(Product product){
+        
+    }
+
 }
