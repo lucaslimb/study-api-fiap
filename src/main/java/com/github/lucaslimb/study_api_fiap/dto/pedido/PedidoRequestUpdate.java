8b362cda6daf761e21b4370fd7190abaf954ecfa
labsfiap <lucasdelimabzr@gmail.com>
2025-05-22 19:54:57 -0300

    correcao pedido status

--- src/main/java/com/github/lucaslimb/study_api_fiap/dto/pedido/PedidoRequestUpdate.java
+++ src/main/java/com/github/lucaslimb/study_api_fiap/dto/pedido/PedidoRequestUpdate.java
@@ -1,21 +1,23 @@
 package com.github.lucaslimb.study_api_fiap.dto.pedido;
 
 import com.github.lucaslimb.study_api_fiap.model.Pedido;
+import com.github.lucaslimb.study_api_fiap.model.PedidoStatus;
 
 public class PedidoRequestUpdate {
 
-    private String status;
+    private PedidoStatus status;
 
     public Pedido toModel(Pedido pedido){
         pedido.setStatus(this.status);
         return pedido;
     }
 
-    public String getStatus() {
+    public PedidoStatus getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(PedidoStatus status) {
         this.status = status;
     }
+
 }
